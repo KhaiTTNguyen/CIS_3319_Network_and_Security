add1f6acc1f09642dc54608865137ad6c7ffddb6
Khai Nguyen <tuh23333@temple.edu>
2020-01-24 16:58:05 -0500

    Using basic C++ implementation

---
+++ Project_1/function.cpp
@@ -0,0 +1,12 @@
+// Khai Nguyen
+// Filename: function.cpp
+// Usage: to build functions
+
+#include "header.h"
+
+#include "DES.hpp" 
+#include "fileDES.hpp"
+
+
+
+// https://www.educative.io/edpresso/how-to-implement-the-des-algorithm-in-cpp
---
+++ Project_1/header.h
@@ -0,0 +1,28 @@
+// Khai Nguyen
+// Filename: header.h
+// Usage to build class and declare functions
+
+#ifndef HEADER_H
+#define HEADER_H
+
+// Include libraries
+#include <iostream>
+#include <fstream> // file opss
+#include <set> // for set operations 
+#include <vector>
+#include <string>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h> // for size_t
+#include <pthread.h>
+#include <sys/socket.h>
+#include <arpa/inet.h> //inet_addr
+#include <unistd.h>
+#include <unordered_set>
+
+using namespace std;
+
+
+
+#endif
